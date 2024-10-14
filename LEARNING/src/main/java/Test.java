4b36cbe042a42ad659769fb78c467d7716ac6275
ex_wuzr11 <ex_wuzr11@partner.midea.com>
2024-10-14 18:13:53 +0800

    [MartinAvex]: init

---
+++ LEARNING/src/main/java/Test.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: some desc
+ * @author: ex_wuzr11
+ * @date: 2024/10/14 18:00
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        System.out.println(map);
+    }
+
+}
