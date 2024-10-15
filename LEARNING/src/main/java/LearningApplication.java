5553a3a812c9c7fcd876ba44ba10559c9135c2a9
ex_wuzr11 <ex_wuzr11@partner.midea.com>
2024-10-15 10:46:08 +0800

    [MartinAvex]: comment

---
+++ LEARNING/src/main/java/LearningApplication.java
@@ -0,0 +1,12 @@
+/**
+ * @description: some desc
+ * @author: ex_wuzr11
+ * @date: 2024/10/14 18:00
+ */
+public class LearningApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
